33322f38d5f420af30238ff406e56be9b311f4af
rabnawaz31 <113094755+rabnawaz31@users.noreply.github.com>
2022-09-08 18:12:40 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Datatypes 
+ {
+   public static void main(String args[])
+   { 
+    byte myByte= 87;
+    short myShort= 1500;
+    int myInt= 65000;
+    long myLong= 6500000;
+    float myFloat= 7.99f;
+    char myChar= 'A';
+    boolean myBoolean= true;
+    String myText= "Hello!!!";
+    
+   System.out.println("Byte = "+myByte);
+   System.out.println("Short = "+myShort);
+   System.out.println("Int = "+myInt);
+   System.out.println("Long = "+myLong);
+   System.out.println("Float = "+myFloat);
+   System.out.println("Char = "+myChar);
+   System.out.println("Boolean = "+myBoolean);
+   System.out.println("String = "+myText);
+   
+   }
+ }
